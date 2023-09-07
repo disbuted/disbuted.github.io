4a4e77bef0af7c9be301a6ab5a670c97576a1316
disbuted <prod.cxr@protonmail.com>
2023-09-07 11:10:19 +0000

    added aimview

--- js/enter.js
+++ js/enter.js
@@ -1,1 +1,12 @@
-var audio=document.getElementById('audio');document.getElementById("enter").addEventListener("click",function(){const target=document.getElementById("enter");target.style.opacity='0';target.addEventListener('transitionend',()=>target.remove());if(typeof window.orientation=='undefined'){audio.play();}});
+document.getElementById("enter").addEventListener("click", function() {
+	var audio = document.getElementById('audio');
+    var video = document.getElementById("bgvid");  
+    const target = document.getElementById("enter");
+    target.style.opacity = '0.5';
+    target.addEventListener('transitionend', () => target.remove());
+    if (typeof window.orientation == 'undefined') {
+        audio.play();
+		video.play();
+    }
+	
+});
