f216fc3470ad99d10e6ce454ecfe0b867fa0aa47
disbuted <prod.cxr@protonmail.com>
2023-10-30 23:56:31 +0000

    updating ggs

--- js/index.js
+++
@@ -1,8 +0,0 @@
-
-let titles = ['abused.lol','private','biolink']
-let index = 0
-let interval = setInterval(() => {
-    document.title = titles[index]
-    index += 1
-    if (index === titles.length) index = 0
-}, 500)
